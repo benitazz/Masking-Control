f0ef026fe2754b3a9ea8725d9ecf4ae7e0b69e21
TRACKER\ben.baloyi <ben.baloyi@tracker.co.za>
2017-06-05 12:04:59 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { MaskInputControlComponent } from './mask-input-control/mask-input-control.component';
+import { NgModule } from '@angular/core';
+import { NumberInputComponent } from './number-input/number-input.component';
+import { TextMaskModule } from 'angular2-text-mask';
+import { TotalInputControlComponent } from './total-input-control/total-input-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberInputComponent,
+    MaskInputControlComponent,
+    TotalInputControlComponent
   ],
+  exports:[ NumberInputComponent,
+    MaskInputControlComponent,
+    TotalInputControlComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TextMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
